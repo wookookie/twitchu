3aded49d530178e0b05cf2a2474dc11b3c48d675
danichoi737 <danichoi737@gmail.com>
2024-02-07 23:51:41 +0900

    Add `morgan` middleware

--- server.js
+++ server.js
@@ -4,10 +4,13 @@
 
 import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
 
 const server = express();
 
 server.set("port", process.env.HTTP_PORT || 8000);
+
+server.use(morgan("dev"));
 
 server.use((req, res, next) => {
   res.send("welcome");
