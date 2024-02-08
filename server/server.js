74e322c21342dc91d4bb6fa88966fc29946459d6
danichoi737 <danichoi737@gmail.com>
2024-02-08 14:35:47 +0900

    Add database features

--- server/server.js
+++ server/server.js
@@ -6,11 +6,20 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import router from "./routes/index.js";
+import database from "./models/index.js";
 
 const server = express();
 
 server.set("port", process.env.HTTP_PORT || 8000);
 server.set("view engine", "ejs");
+
+try {
+  await database.sequelize.sync({ force: true });
+  console.log("Database connected");
+}
+catch (error) {
+  console.error(error);
+}
 
 server.use(morgan("dev"));
 server.use(express.static("public"));
