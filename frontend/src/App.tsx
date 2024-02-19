20d3369005420fff69d1197c2de98a01d3ab7d98
danichoi737 <danichoi737@gmail.com>
2024-02-20 07:56:16 +0900

    Apply `chakra-ui`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@chakra-ui/button";
+import { Box, HStack, Heading, VStack } from "@chakra-ui/layout";
+
 function App() {
-  return <></>;
+  return (
+    <Box display="flex" justifyContent="center">
+      <VStack>
+        <Heading as="h1">Welcome to Twitchu!</Heading>
+        <HStack marginTop={5}>
+          <Button colorScheme="blue">Sign in</Button>
+          <Button colorScheme="blue">Sign up</Button>
+        </HStack>
+      </VStack>
+    </Box>
+  );
 }
 
 export default App;
