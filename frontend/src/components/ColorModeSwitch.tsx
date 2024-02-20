408cbf09ae9fae18624ce44f7954347dac231c70
danichoi737 <danichoi737@gmail.com>
2024-02-20 13:29:43 +0900

    Add color mode icons

--- frontend/src/components/ColorModeSwitch.tsx
+++ frontend/src/components/ColorModeSwitch.tsx
@@ -1,15 +1,22 @@
 import { useColorMode } from "@chakra-ui/color-mode";
+import { Icon } from "@chakra-ui/icon";
+import { HStack } from "@chakra-ui/layout";
 import { Switch } from "@chakra-ui/switch";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 function ColorModeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Switch
-      colorScheme="pink"
-      isChecked={colorMode === "dark"}
-      onChange={toggleColorMode}
-    ></Switch>
+    <HStack>
+      <Icon as={FiSun} />
+      <Switch
+        colorScheme="pink"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      ></Switch>
+      <Icon as={FiMoon} />
+    </HStack>
   );
 }
 
